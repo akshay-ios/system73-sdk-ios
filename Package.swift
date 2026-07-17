f139205fe8c29fa4b1ec7d344696519bf28d199d
akshay-ios <113525781+akshay-ios@users.noreply.github.com>
2026-07-18 02:03:35 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,42 +1,61 @@
-// swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "system73-sdk-ios",
+    name: "system73-sdk-ios-spm",
+
     products: [
-            // Products define the executables and libraries a package produces, making them visible to other packages.
-        // testing with xcode26.3
-            .library(
-                name: "PolyNetSDK",
-                targets: ["PolyNetSDK"]
-            ),
-            .library(
-                name: "Starscream",
-                targets: ["Starscream"]
-            ),
-            .library(
-                name: "SwiftProtobuf",
-                targets: ["SwiftProtobuf"]
+        .library(
+            name: "PolyNetSDK",
+            targets: ["PolyNetSDKWrapper"]
+        )
+    ],
+
+    dependencies: [
+        .package(
+            url: "https://github.com/daltoniam/Starscream.git",
+            from: "4.0.8"
+        ),
+        .package(
+            url: "https://github.com/apple/swift-protobuf.git",
+            from: "1.26.0"
+        ),
+        .package(
+            url: "https://github.com/System73/system73-webrtc-ios-spm.git",
+            from: "1.0.0"
+        ),
+        .package(
+            url: "https://github.com/System73/system73-webrtc-tvos-spm.git",
+            from: "1.0.0"
+        )
+    ],
+
+    targets: [
+
+        .binaryTarget(
+            name: "PolyNetSDK",
+            url: "https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-apple-sdk/5.2.3/polynet-apple-sdk-5.2.3.zip",
+            checksum: "baeb71e53e352c388df6e66bf2e32264e380efcf84306d1a3c3945ff075d5560"
+        ),
+
+            .target(
+                name: "PolyNetSDKWrapper",
+                dependencies: [
+                    "PolyNetSDK",
+                    .product(name: "Starscream", package: "Starscream"),
+                    .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                    .product(
+                        name: "WebRTC_iOS",
+                        package: "system73-webrtc-ios-spm",
+                        condition: .when(platforms: [.iOS])
+                    ),
+                    .product(
+                        name: "WebRTCtvOS",
+                        package: "system73-webrtc-tvos-spm",
+                        condition: .when(platforms: [.tvOS])
+                    )
+                ],
+                path: "Sources/system73-sdk-ios-spm"
             )
-        ],
-        dependencies: [],
-        targets: [
-            .binaryTarget(
-                name: "PolyNetSDK",
-                url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-apple-sdk/5.2.2/polynet-apple-sdk-5.2.2.zip",
-                checksum:"3cc60b1ea903140aa38d5da8beb313817492ac02692bf799a817d808ecf6f141"
-            ),
-            .binaryTarget(
-                name: "Starscream",
-                url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-ios-starscream-xcframework/4.0.8.11.02.2026/polynet-ios-starscream-xcframework-4.0.8.11.02.2026.zip",
-                checksum: "4b590f7e13a302966ce647e9ef3caa62652eab53dd290bc540560987a886f767"
-            ),
-            .binaryTarget(
-                name: "SwiftProtobuf",
-                url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-ios-swift-protobuf-xcframework/1.26.0.11.02.2026/polynet-ios-swift-protobuf-xcframework-1.26.0.11.02.2026.zip",
-                checksum: "201d4700e4fac2b9b71a28164b17ce7f8dbd402d70c34f9f7e6d761a3bed2db7"
-            )
-        ]
+    ]
 )
+
